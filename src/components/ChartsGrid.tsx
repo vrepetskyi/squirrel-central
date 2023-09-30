7cf4ae73a2c01a2faf1b0dd2d629202be0905f65
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-09-30 10:11:00 +0200

    Added color bar chart

--- src/components/ChartsGrid.tsx
+++ src/components/ChartsGrid.tsx
@@ -1,23 +1,43 @@
 import { count } from "d3";
 import EChartsReact from "echarts-for-react";
 import React from "react";
-import { Activity, Interaction, type Observation } from "~/utils";
+import { Activity, Color, Interaction, type Observation } from "~/utils";
 import { DensityPlot } from "./DensityPlot";
 import { WithCaption } from "./WithCaption";
 
+const colorValues = Object.values(Color).slice(4, 8);
 const activityValues = Object.values(Activity).slice(5, 10);
 const interactionValues = Object.values(Interaction).slice(8, 16);
 
 const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
-  const altitude = filtered
+  const altitudeDensity = filtered
     .map((x) => x.location.z)
     .filter((x) => x !== undefined)
     .map((x) => x! * 0.3048);
 
+  const colorPie = colorValues
+    .map((color) => ({
+      name: Color[color as Color],
+      value: count(filtered, (x) =>
+        [x.color.primary, ...x.color.highlights].includes(color as Color)
+          ? 1
+          : null,
+      ),
+    }))
+    .filter((x) => x.value);
+
+  const colorBar = new Map();
+  filtered.forEach((x) => {
+    const number = x.color.primary ? 1 : 0 + x.color.highlights.length;
+    colorBar.set(number, (colorBar.get(number) || 0) + 1);
+  });
+
+  console.log(colorBar);
+
   const juveniles = count(filtered, (x) => (x.isJuvenile === true ? 1 : null));
   const adults = count(filtered, (x) => (x.isJuvenile === false ? 1 : null));
 
-  const age = [
+  const agePie = [
     {
       name: "Unknown",
       value: filtered.length - juveniles - adults,
@@ -32,7 +52,7 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
     },
   ].filter((x) => x.value);
 
-  const activities = activityValues
+  const activityPie = activityValues
     .map((activity) => ({
       name: Activity[activity as Activity],
       value: count(filtered, (x) =>
@@ -41,7 +61,7 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
     }))
     .filter((x) => x.value);
 
-  const interactions = interactionValues
+  const interactionPie = interactionValues
     .map((interaction) => ({
       name: Interaction[interaction as Interaction],
       value: count(filtered, (x) =>
@@ -60,9 +80,46 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
         <DensityPlot
           width={400}
           height={200}
-          data={altitude}
+          data={altitudeDensity}
           min={-5}
           max={60}
+        />
+      </WithCaption>
+      <WithCaption caption="Color distribution">
+        <EChartsReact
+          className="w-[400px]"
+          theme="dark"
+          option={{
+            series: [
+              {
+                type: "pie",
+                data: colorPie,
+              },
+            ],
+          }}
+          opts={{ renderer: "svg" }}
+        />
+      </WithCaption>
+      <WithCaption caption="Number of colors">
+        <EChartsReact
+          className="w-[400px]"
+          theme="dark"
+          option={{
+            xAxis: {
+              type: "category",
+              data: [...colorBar.keys()],
+            },
+            yAxis: {
+              type: "value",
+            },
+            series: [
+              {
+                type: "bar",
+                data: [...colorBar.values()],
+              },
+            ],
+          }}
+          opts={{ renderer: "svg" }}
         />
       </WithCaption>
       <WithCaption caption="Age distribution">
@@ -73,7 +130,7 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
             series: [
               {
                 type: "pie",
-                data: age,
+                data: agePie,
               },
             ],
           }}
@@ -88,7 +145,7 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
             series: [
               {
                 type: "pie",
-                data: activities,
+                data: activityPie,
               },
             ],
           }}
@@ -103,7 +160,7 @@ const ChartsGrid: React.FC<{ filtered: Observation[] }> = ({ filtered }) => {
             series: [
               {
                 type: "pie",
-                data: interactions,
+                data: interactionPie,
               },
             ],
           }}
